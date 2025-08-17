27acad141eefe052b509551b80da8aa36bc84aa7
Mike Meyiswa <nmeyiswa@gmail.com>
2025-08-17 05:58:02 +0000

    the firebase is called App-Mobi but i cant seem to configure it

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,8 +3,24 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/setup#config-object
+// ====================================================================================
+// TODO: PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
+// ====================================================================================
+// You can get this from the Firebase console for your "App-Mobi" project.
+// Go to Project Settings > General > Your apps > SDK setup and configuration.
+// Select "Config" and copy the entire firebaseConfig object.
+//
+// It should look like this:
+//
+// const firebaseConfig = {
+//   apiKey: "AIza...",
+//   authDomain: "app-mobi.firebaseapp.com",
+//   projectId: "app-mobi",
+//   storageBucket: "app-mobi.appspot.com",
+//   messagingSenderId: "1234567890",
+//   appId: "1:1234567890:web:abcde..."
+// };
+//
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
@@ -13,6 +29,10 @@ const firebaseConfig = {
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
+// ====================================================================================
+// END OF FIREBASE CONFIGURATION
+// ====================================================================================
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
